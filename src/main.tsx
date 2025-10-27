37db3fb92e4055d56cabcb4e161973ee8ac3171e
Taskov1ch <qq.tynaev@gmail.com>
2025-10-27 16:52:54 +0600

    fix: возвращение адресной строки для разработчиков

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
-import { RPCProvider } from "./contexts/RPCContext";
-import "./App.css";
+import { DevAddressBar } from "./components/DevAddressBar/DevAddressBar";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<RPCProvider>
+import {
+	loadAppZoom,
+	applyAppZoom,
+	loadThemePreference,
+	applyTheme,
+	listenToSystemThemeChanges
+} from "./utils/settingsStore";
+
+const startApp = async () => {
+	const zoom = await loadAppZoom();
+	applyAppZoom(zoom);
+
+	const themePreference = await loadThemePreference();
+	applyTheme(themePreference);
+	listenToSystemThemeChanges(themePreference);
+
+	const isDevelopment = import.meta.env.DEV;
+
+	ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+		<React.StrictMode>
+			<BrowserRouter>
+				{isDevelopment && <DevAddressBar />}
 				<App />
-			</RPCProvider>
-		</BrowserRouter>
-	</React.StrictMode>
-);
+			</BrowserRouter>
+		</React.StrictMode>
+	);
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+	startApp();
+});
